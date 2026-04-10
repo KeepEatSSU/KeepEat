8f019ed32907c6aeb639cadb94ec9f71ee38c4e0
Kim Ji Seong <rlawltjd3240@naver.com>
2026-04-10 21:36:50 +0900

    Revert "Feature/be/user profile"

--- backend/src/main/java/com/keepeat/backend/domain/ingredient/IngredientStorageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.keepeat.backend.domain.ingredient;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientStorageRepository extends JpaRepository<IngredientStorage, Long> {
-}
